d07a5056394da1d12d91d1d44fb683617c776b32
Duane Snelling <duane.snelling@icloud.com>
2016-08-05 13:59:19 -0400

    added outlets
    added outlets and called model

--- TipCalculator/TipCalculatorVC.swift
+++ TipCalculator/TipCalculatorVC.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class TipCalculatorVC: UIViewController {
 
+    // MARK: - @IBOutlet
+    
+    @IBOutlet weak var tipCalculateTileView: UIView!
+    @IBOutlet weak var tipCalculateTileLbl: UILabel!
+    @IBOutlet weak var billAmountLbl: UITextField!
+    @IBOutlet weak var tipPercentLbl: UILabel!
+    @IBOutlet weak var tipPercentSlider: UISlider!
+    @IBOutlet weak var tipAmountLbl: UILabel!
+    @IBOutlet weak var totalAmountLbl: UILabel!
+    @IBOutlet weak var splitPeopleLbl: UILabel!
+    @IBOutlet weak var splitSlider: UISlider!
+    @IBOutlet weak var splitAmountLbl: UILabel!
+    
+    // MARK: - @Properties
+    var tipCalculate = TipCalculate(billAmount: 0.0, tipPercent: 0.0, splitPeople: 0)
+    
+    // MARK: - Initialize Views
     override func viewDidLoad() {
         super.viewDidLoad()
         
     
     }
 
-
+    // MARK: - @IBActions
+    
+    //MARK: - Functions
 }
 
